9bbbb5fe98745635cdf3cbe11fb90feb7f2bb2a3
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-08 00:27:56 +0100

    FIX: Change order of quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
     before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.order(updated_at: :desc)
   end
 
   def show; end
