5b45e8f3eeb209e53de4117aff1d4ec625f0aac8
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-09 01:47:21 +0100

    RFCT: Refactor after_create_commit

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,5 +4,5 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # AfterCreateCommit has to do with running only after a successful db transaction after create
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
 end
