feb3422a465d3a1137b11bfe3fced3df25ed85d0
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-09 01:45:53 +0100

    FEAT: Make quote creation real time

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,7 @@ class Quote < ApplicationRecord
   validates :name, uniqueness: true, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # AfterCreateCommit has to do with running only after a successful db transaction after create
+  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
