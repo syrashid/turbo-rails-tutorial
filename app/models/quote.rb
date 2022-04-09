6998676bb8fa104dc2a357359402d03bba13535c
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-09 01:54:34 +0100

    RFCT: Make create and update actions bg jobs

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,5 +4,7 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # AfterCreateCommit has to do with running only after a successful db transaction after create
-  after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
