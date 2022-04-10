7026cb9b9234b1c2e846c40a5e664ecbab75bcaa
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-10 17:37:05 +0100

    FEAT: Add back in current company helper method

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
     before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.ordered
+    @quotes = current_company.quotes.ordered
   end
 
   def show; end
--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = Quote.ordered.first
+    login_as users(:accountant)
+    @quote = quotes(:first)
   end
 
   test "Creating a new quote" do
