f8378c394ea9d05f5588686cab89f20b3b273733
Sy Rashid <sy.rashid.11@gmail.com>
2022-04-24 17:26:06 +0100

    FEAT: Add footer for quotes to display total

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -14,4 +14,8 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
